dec0cfc1052acddac0ef39f43262a2517e7db616
romeo adjam <sodangbe@users.noreply.github.com>
2021-05-24 18:21:50 -0500

    adding css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,82 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+
+  password = '';
+  includeLetters = false ;
+  includeNumbers = false ;
+  includeSymbols = false ;
+  length = 0;
+
+
+
+  onChangeLength(value : string){
+
+    const parsedValue = parseInt(value);
+
+    if(!isNaN(parsedValue)){
+
+      this.length = parsedValue;
+
+    }
+
+  }
+
+  onChangeUseLetters(){
+
+    this.includeLetters = !this.includeLetters;
+
+  }
+
+  onChangeUseNumbers(){
+
+    this.includeNumbers = !this.includeNumbers;
+    ;
+
+  }
+
+  onChangeUseSymbols(){
+
+    this.includeSymbols = !this.includeSymbols;
+
+  }
+
+  onButtonClick(){
+
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz'
+    const symbols = '!@#$%^&*()'
+
+    let validChar = '';
+
+    if(this.includeLetters){
+      validChar += letters;
+    }
+
+    if(this.includeNumbers){
+      validChar += numbers ;
+    }
+
+    if(this.includeSymbols){
+      validChar += symbols ;
+    }
+
+    let generatedPassword ='';
+
+    for(let i= 0; i< this.length; i++){
+
+      const index = Math.floor(Math.random() * validChar.length);
+      generatedPassword += validChar[index];
+    }
+
+    this.password = generatedPassword ;
+
+  }
+
+  getPassword(){
+
+    return this.password;
+  }
+
+
 }
